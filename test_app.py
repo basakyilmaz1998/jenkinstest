87dd98d6d125d6626d8133ae90f194275fadcd4e
Başak Yılmaz <129073381+basakyilmaz1998@users.noreply.github.com>
2025-12-10 21:11:50 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,11 +1,12 @@
 import time
 import requests
 
-url = "https://example.com"
+def test_url_access():
+    url = "https://example.com"
 
-r1 = requests.get(url)
-time.sleep(2)
-r2 = requests.get(url)
+    r1 = requests.get(url)
+    time.sleep(2)
+    r2 = requests.get(url)
 
-assert r1.url == r2.url, "2. request farklı bir URL'e gitti!"
-print("Test geçti: 2 saniye sonra aynı URL'e gidildi.")
+    assert r1.status_code == 200
+    assert r2.status_code == 200
